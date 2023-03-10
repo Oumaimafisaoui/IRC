753f4c32af2fdb0bc2b13ae5c8a520f80b05c3e6
Oumaima Fisaoui <oufisaou@e1r13p3.1337.ma>
2023-03-10 18:39:22 +0100

    change the \r

--- server.cpp
+++ server.cpp
@@ -109,8 +109,8 @@ void Server::launch_socket()
 void Server::receive_message(std::vector<pollfd>::iterator i, Client *client, int len)
 {
     this->message = "";
-    
     buffer[len] = 0;
+
     if (len < 0)
         return ;
     else
@@ -126,7 +126,7 @@ void Server::receive_message(std::vector<pollfd>::iterator i, Client *client, in
         {
             this->buffer[len] = 0;
             message.append(buffer);
-            std::size_t j = message.find("\n\r");
+            std::size_t j = message.find("\r\n");
             while(j != message.npos)
             {
                 //remove "\r"
@@ -135,9 +135,9 @@ void Server::receive_message(std::vector<pollfd>::iterator i, Client *client, in
             }
         }
     }
- 
-// client_not_connected(message ,client);
-client_connected(message, client);
+// check for client connection status
+client_not_connected(message ,client);
+// client_connected(message, client);
 }
 
 
@@ -218,6 +218,7 @@ void Server::client_not_connected(std::string message , Client *client)
                 key = message_split[k].substr(pos_it, message_split[k].length() - pos_it);
                 command_split.push_back(key);
         }
+        //execute
         k++; 
    }
 
@@ -237,19 +238,15 @@ Server::Server(int port, std::string password): password(password), port(port) ,
     //create the server socket
     this->launch_socket();
 
-    //Create the pollfd structure
-    //pollfd is a structure represent a file descriptor to be monitored by the poll() system call.
     pollfd server_poll;
     std::memset(&server_poll, 0, sizeof(server_poll)); //initialize the structure with zeros
 
 
-    //client adrr infos
     struct sockaddr_in addr_client;
     socklen_t len = sizeof(addr_client);
     struct pollfd client_poll;
     int client_fd;
 
-    //this configuration for the server socket, we want to poll for incoming data on the server socket
     server_poll.fd = this->fd;
     server_poll.events = POLLIN; //should monitor for incoming data
 
@@ -257,18 +254,10 @@ Server::Server(int port, std::string password): password(password), port(port) ,
 
     while(this->off == FALSE)
     {
-        //this is the poll function that is used to monitor each connection
-        //poll_vec.data() => a pointer to the first element of poll_vec, list of fds to be monitored
-        //this->poll_vec.size() => the number of elements in poll_vec, rn is 1
-        // the amount of time to wait for an event to accure
         if(poll(poll_vec.data(), this->poll_vec.size(), 0) < 0)
             throw Server::ProblemInPoll();
-
-         //an iterator that points on the elements of the pollfd vector
-        //iterate through the poll_vec
         for (unsigned long i = 0; i < sizeof(poll_vec); i++)
         {
-            //skip the current itteration if there is no incoming data on the current socket
             pollfd& current = poll_vec[i];
             if (!(current.revents & POLLIN))
                 continue;
@@ -276,24 +265,20 @@ Server::Server(int port, std::string password): password(password), port(port) ,
                 {
                    try
                    {
-                         //accept the incoming client connection
+                    
                         client_fd = accept(this->fd, (struct sockaddr*)&addr_client,&len);
                         if (client_fd < 0)
                             throw std::runtime_error("Problem in accept client: ");
                         fcntl(client_fd, F_SETFL, O_NONBLOCK); //sets the client socket to non-blocking
-                        //fill the client's pollfd struct with infos on the client
+                
                         client_poll.fd = client_fd;
                         client_poll.events = POLLIN; //monitor incoming data on client
-                        //In the next iteration, the poll function will update
-                        //the revents field to reflect the new events that have occurred since the last call to poll
-                        //If we don't clear the revents field, the previous events
-                        // will still be present and may cause incorrect behavior in our program.
-                    
-                        client = new Client(fd);
-                        //adds the client_poll structure to the poll_vec vector
+            
+                        client = new Client(poll_vec[i].fd);
+                        client->setFd(poll_vec[i].fd);
+                       
                         poll_vec.push_back(client_poll);
                         std::cout << "pushed to the vector" << std::endl;
-                        client->setFd(fd);
                    }
                    catch(const std::exception& e)
                    {
@@ -304,12 +289,6 @@ Server::Server(int port, std::string password): password(password), port(port) ,
                 }
                 else 
                 {
-                    //If the event was on the server socket, 
-                    //a new client connection is accepted using the accept() function,
-                    //and the client socket is added to the poll_vec vector. 
-                    //If the event was on a client socket, 
-                    //the server calls the receive_message() function 
-                    //to handle the incoming data.
                     int len = recv(poll_vec[i].fd, this->buffer, 500, 0);
                     receive_message(i + poll_vec.begin(), client, len);    
                 }
